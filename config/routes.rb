7991935fafb23fed3e952401a5c280ff8ddb40ab
Jarrod Mosen <jarrod.mosen@me.com>
2014-04-24 22:13:48 +1200

    Temp lame shit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   as :user do
     get '/login' => 'devise/sessions#new'
     get '/logout' => 'devise/sessions#destroy'
+    get '/register' => 'devise/registrations#new'
   end
 
   root to: 'home#index'
