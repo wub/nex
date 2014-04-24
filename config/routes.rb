ecdb287fd03549a668f4c47ea31882300f52dbd0
Jarrod Mosen <jarrod.mosen@me.com>
2014-04-25 02:14:09 +1200

    Adding/editing some devise views and routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     get '/login' => 'devise/sessions#new'
     get '/logout' => 'devise/sessions#destroy'
     get '/register' => 'devise/registrations#new'
+    get '/profile/edit' => 'devise/registrations#edit'
   end
+
+  get 'profile', to: 'users#index'
+  get '/profile/:id', to: 'users#show'
 
   root to: 'home#index'
 end
