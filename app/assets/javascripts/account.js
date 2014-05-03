1d553ca69c4a8dffb8d25c5898b2ac6fae9bb795
Jarrod Mosen <jarrod.mosen@me.com>
2014-05-04 02:52:57 +1200

    JS tidy

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,7 +1,13 @@
-var uamodal = document.getElementById('modal-account'),
-    overlay = document.getElementById('modal-overlay'),
-    account = document.getElementById('account');
+(function(){
 
-uamodal.addEventListener('click', function(e) { e.stopPropagation(); });
-account.addEventListener('click', function( ) { overlay.hidden = false; document.body.style.overflow = 'hidden' });
-overlay.addEventListener('click', function( ) { overlay.hidden = true;  document.body.style.overflow = 'auto'   });
+  'use strict';
+
+  var uamodal = document.getElementById('modal-account'),
+      overlay = document.getElementById('modal-overlay'),
+      account = document.getElementById('account');
+
+  uamodal.addEventListener('click', function(e) { e.stopPropagation(); });
+  account.addEventListener('click', function( ) { overlay.hidden = false; document.body.style.overflow = 'hidden' });
+  overlay.addEventListener('click', function( ) { overlay.hidden = true;  document.body.style.overflow = 'auto'   });
+
+})();
--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -1,40 +1,43 @@
-'use strict';
+(function(){
 
-var meta = document.getElementsByTagName('meta'),
-		meta_length = meta.length,
-		csrf;
+	'use strict';
 
-for (var i = 0; i < meta_length; i++) {
-	if (meta[i].getAttribute('name') == 'csrf-token') {
-	 csrf = meta[i].getAttribute('content');
+	// This is all to get the content of a meta tag... wow
+	var meta = document.getElementsByTagName('meta'),
+      meta_length = meta.length,
+      csrf;
+
+	for (var i = 0; i < meta_length; i++) {
+		if (meta[i].getAttribute('name') == 'csrf-token') {
+			csrf = meta[i].getAttribute('content');
+		}
 	}
-}
 
-var r = new Resumable({
-	target: '/chunk',
-	headers: { 'X-CSRF-Token': csrf },
-	prioritizeFirstAndLastChunk: true, // so we can abandon early
-	maxChunkRetries: 50,
-	minFileSize: 1,
-});
+	var r = new Resumable({
+		target: '/chunk',
+		headers: { 'X-CSRF-Token': csrf },
+		prioritizeFirstAndLastChunk: true, // so we can abandon early
+		maxChunkRetries: 50,
+		minFileSize: 1,
+	});
 
-// Unsupported; fallback
-if (! r.support) location.href = '/upload'
-else console.log(r);
+	// Unsupported; fallback
+	if (! r.support) console.error('Too old go away');
 
-r.assignBrowse(document.getElementById('browseButton'));
-r.assignDrop(document.getElementById('dropTarget'));
+	r.assignBrowse(document.getElementById('browseButton'));
+	r.assignDrop(document.getElementById('dropTarget'));
 
-r.on('fileAdded', function(file, event) {
-	console.log(file);
-	r.upload();
-});
+	r.on('fileAdded', function(file, event) {
+		console.log(file);
+		r.upload();
+	});
 
-r.on('fileSuccess', function(file, message) {
-	console.log(message);
-});
+	r.on('fileSuccess', function(file, message) {
+		console.log(message);
+	});
 
-r.on('fileError', function(file, message) {
-	console.log(file, message);
-	console.log('DOOM');
-});
+	r.on('fileError', function(file, message) {
+		console.log(file, message);
+	});
+
+})();
